76359e065f85fe0e108fe9f28d240462a2b136a3
Hòa Nguyễn <shin.l0v3.ly@gmail.com>
2021-06-02 20:58:33 +0700

    add appsetings

--- Program.cs
+++ Program.cs
@@ -2,33 +2,30 @@
 using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Storage.V1;
 using Google.Cloud.Translate.V3;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.IO;
 
 namespace ConsoleApp1
 {
     class Program
     {
-
-        public object AuthImplicit(string projectId)
-        {
-            // If you don't specify credentials when constructing the client, the
-            // client library will look for credentials in the environment.
-            var credential = GoogleCredential.GetApplicationDefault();
-            var storage = StorageClient.Create(credential);
-            // Make an authenticated API request.
-            var buckets = storage.ListBuckets(projectId);
-            foreach (var bucket in buckets)
-            {
-                Console.WriteLine(bucket.Name);
-            }
-            return null;
-        }
-
         static void Main(string[] args)
         {
             try
             {
-
+                var value = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+                if (value == null)
+                {
+                    Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", $"{Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json")}");
+                    // Now retrieve it.
+                    value = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+                }
+                var builder = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json");
+                var configuration = builder.Build();
+                var project_id = configuration["project_id"];
                 //var credential = GoogleCredential.GetApplicationDefault();
                 //var storage = StorageClient.Create(credential);
                 //// Make an authenticated API request.
@@ -40,13 +37,12 @@ namespace ConsoleApp1
 
                 Console.WriteLine("Nhập từ muốn dịch: ");
                 String txt = Console.ReadLine();
-
                 TranslationServiceClient client = TranslationServiceClient.Create();
                 TranslateTextRequest request = new TranslateTextRequest
                 {
                     Contents = { $"{txt}" },
                     TargetLanguageCode = "vi-VI",
-                    Parent = new ProjectName("ggtranslatorapi").ToString()
+                    Parent = new ProjectName($"{project_id}").ToString()
                 };
                 TranslateTextResponse response = client.TranslateText(request);
                 // response.Translations will have one entry, because request.Contents has one entry.
